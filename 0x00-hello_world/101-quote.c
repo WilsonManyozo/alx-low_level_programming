f8983ea483477617e6a370486dc378ee1a7ec2fb
WilsonManyozo <wilsonmanyozo5@gmail.com>
2023-03-19 12:18:49 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <Unistdio.h>
+/**
+ * main -a c prodram that print a line to a standard error
+ * Return: 1(success)
+ */
+int main(void)
+{
+	("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", stdout);
+
+return (1);
