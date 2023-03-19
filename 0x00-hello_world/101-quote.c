1751f2a69e668cb96af576a5caa1fa1461d87849
WilsonManyozo <wilsonmanyozo5@gmail.com>
2023-03-19 12:50:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	print("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", stdout);
+	printf("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", stdout);
 	
 return (1);
 
