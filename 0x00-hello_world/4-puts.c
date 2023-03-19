6955bbcd06d078269e48d96b1905d595bf19af06
WilsonManyozo <wilsonmanyozo5@gmail.com>
2023-03-19 10:21:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - "Programming is like building a multilingual puzzle
- *
- * Return: 0(Success)
+ * main -print "Programming is like building a multilingual puzzle
+ * Return: always 0
  */
 int main(void)
+
 {
-puts("Programming is like building a multilingual puzzle)",
+	put("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
