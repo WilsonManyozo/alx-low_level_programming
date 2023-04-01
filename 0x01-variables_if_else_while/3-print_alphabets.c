0b1891c27ec7faf0ae482b7fd877bb5b3983332c
WilsonManyozo <wilsonmanyozo5@gmail.com>
2023-04-01 15:01:04 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,10 @@ int main(void)
 {
 	char low;
 
-	for (low = 'A'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++
+			putchar(low);
+
+	for (low = 'A'; low <= 'Z'; low++)
 	putchar(low);
 
 	return (0);
