d60444f405deb47ba984d266765f44dd08c8c985
WilsonManyozo <wilsonmanyozo5@gmail.com>
2023-04-01 13:17:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the alphabetic
+ * Retuen: ALways (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
