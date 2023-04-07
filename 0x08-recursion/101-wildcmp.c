411b8c24baa3722f00cc622e6d1fcbe79efa5c13
WilsonManyozo <wilsonmanyozo5@gmail.com>
2023-04-07 09:37:30 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,7 +19,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == *s1)
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1));
 	else
 		return (0);
 }
