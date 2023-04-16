3260234010c9a52f7e960b5383ae077f955b4a79
WilsonManyozo <wilsonmanyozo5@gmail.com>
2023-04-16 03:12:39 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include "main.h"
 /**
  * _strlen - calculate and return string length
  * @string: string
@@ -13,7 +13,6 @@ int _strlen(char *string)
 	for (i = 0; string[i] != '\0'; i++)
 		return (i);
 }
-
 /**
  * string_nconcat - concatenate s1 and n bytrs of s2; return ptr to string
  * @s1: string 1
@@ -21,7 +20,7 @@ int _strlen(char *string)
  * @n: n bytes to concat from string 2
  * Return: pointer to concatenated string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
 	int num, len, i, j;
@@ -37,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 	if (num >= _strlen(s2)) /* account for n too big */
 		num = _strlen(s2);
 
-	len = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	len =  malloc(sizeof(*ptr) * len); /* malloc and check for error */
 	if (ptr == NULL)
 		return (NULL);
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _putchar(char *str);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _strlen(char *string)
+void _puts(char *str);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
 
 #endif
