39a664232c1689ea57a2bb913795d60eb45bad47
WilsonManyozo <wilsonmanyozo5@gmail.com>
2023-04-18 03:49:24 -0700

    all tasks of 0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ * Return: 0 Alwyas
+ */
+
+int main(void)
+
+{
+	int _FILE_;
+
+		printf("%s\n", _FILE_);
+
+	return (0);
+}
