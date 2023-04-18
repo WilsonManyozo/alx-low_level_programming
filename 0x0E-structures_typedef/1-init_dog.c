c2f6caeceb4b37d77f35633c747c870f6c4a071e
WilsonManyozo <wilsonmanyozo5@gmail.com>
2023-04-18 13:08:22 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,9 +13,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
-	free(name);
-	free(age);
-	free(owner);
-
-	return (0);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
